8168428e3ac728b19d29ec0731635fd52f8d4d3e
elormd <dziks20052@gmail.com>
2020-10-16 13:24:21 +0000

    this is exercise 17

---
+++ seventh.py
@@ -0,0 +1,9 @@
+age = 20
+if age < 18:
+    print('You are old enough to vote.')
+else:
+    print('Welcome to our voting program.')
+if age >= 18:
+    print('Welcome to our voting program.')
+else:
+    print('you are not old enough to vote')
